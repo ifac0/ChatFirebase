c4ce4d2add4a3a1aae1dd358dbc219211e6e5513
Ivan Costa <ivanfelipealves@gmail.com>
2019-07-19 22:43:44 -0300

    Upload de Foto para o Firebase

--- app/src/main/java/br/com/chatfirebase/RegisterActivity.java
+++ app/src/main/java/br/com/chatfirebase/RegisterActivity.java
@@ -1,21 +1,35 @@
 package br.com.chatfirebase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -23,6 +37,9 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mEditEmail;
     private EditText mEditPassword;
     private Button mBtnCadastrar;
+    private Button mBtnSelectPhoto;
+    private Uri mSelectedUri;
+    private ImageView mImgPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,21 +50,55 @@ public class RegisterActivity extends AppCompatActivity {
         mEditEmail = findViewById(R.id.editEmail);
         mEditPassword = findViewById(R.id.editPassword);
         mBtnCadastrar = findViewById(R.id.btnCadastrar);
+        mBtnSelectPhoto = findViewById(R.id.btnSelectedPhoto);
+        mImgPhoto = findViewById(R.id.imagePhoto);
+
+        mBtnSelectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectPhoto();
+            }
+        });
 
         mBtnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                createUser();
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        if(requestCode == 0) {
+            mSelectedUri = data.getData();
+
+            Bitmap bitmap = null;
+            try{
+                bitmap =  MediaStore.Images.Media.getBitmap(getContentResolver(), mSelectedUri);
+                mImgPhoto.setImageDrawable(new BitmapDrawable(bitmap));
+                mBtnSelectPhoto.setAlpha(0);
+            }catch (IOException e){
+            }
+
+        }
+    }
+
+    private void selectPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+    }
+
     private  void createUser() {
+        String nome = mEditUsername.getText().toString();
         String email  =  mEditEmail.getText().toString();
         String senha = mEditEmail.getText().toString();
 
-        if( email == null || email.isEmpty() || senha.isEmpty() ){
-            Toast.makeText(this, "Senha e Email deve ser preenchiidos!", Toast.LENGTH_LONG).show();
+        if( nome == null || nome.isEmpty() || email == null || email.isEmpty() || senha.isEmpty() ){
+            Toast.makeText(this, "Nome, Senha e Email deve ser preenchiidos!", Toast.LENGTH_LONG).show();
             return;
         }
 
@@ -55,13 +106,39 @@ public class RegisterActivity extends AppCompatActivity {
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.i("Erro", task.getResult().getUser().getUid());
+                        if(task.isSuccessful()) {
+                            Log.i("Erro", task.getResult().getUser().getUid());
+                            
+                            saveUserInFirebase();
+                        }
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.i("Erro", e.getMessage());
+                    }
+                });
+    }
+
+    private void saveUserInFirebase() {
+        String filename = UUID.randomUUID().toString();
+        final StorageReference ref = FirebaseStorage.getInstance().getReference("/images/" + filename);
+        ref.putFile(mSelectedUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Log.i("Erro", uri.toString());
+                            }
+                        });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("Erro", e.getMessage(), e);
                     }
                 });
     }
